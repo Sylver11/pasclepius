077c94bc8f1f0370793fb375599945a7e77ca979
Sylver11 <justus@cognician.com>
2020-04-11 22:57:08 +0200

    dynamic list gen

--- forms.py
+++ forms.py
@@ -1,23 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, TextField, SubmitField, FieldList
-from wtforms.validators import DataRequired, Length, Email
-
-
-
-
-
-class ContactForm(FlaskForm):
-    """Contact form."""
-    name = StringField('Name', [
-        DataRequired()])
-    email = StringField('Email', [
-        Email(message=('Not a valid email address.')),
-        DataRequired()])
-    body = TextField('Message', [
-        DataRequired(),
-        Length(min=4, message=('Your message is too short.'))])
-    recaptcha = RecaptchaField()
-    submit = SubmitField('Submit')
+from wtforms import StringField, TextField, SubmitField, FieldList, SelectField
+from wtforms.validators import DataRequired, Length, Email, Required
 
 
 
@@ -25,3 +8,5 @@ class Sample(FlaskForm):
     name = StringField()
     flist = FieldList(TextField())
     submit = SubmitField('Submit')
+    myChoices = [(1,"hello"),(2, "hello again"),(3, "goodbye")]
+    myField = SelectField('Treatment', choices = myChoices, validators = [Required()])
