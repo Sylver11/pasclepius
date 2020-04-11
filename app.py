9d7845a518e6bf1e11c5303b7cfe74c9bc48d6e2
Sylver11 <justus@cognician.com>
2020-04-11 17:59:54 +0200

    add basic form jinja

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, Response, request, session
+from jinja2 import Template
+from forms import Sample
 
 
 app = Flask(__name__)
@@ -6,9 +8,12 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
-def hello():
-    return 'Justus helllloo'
+@app.route('/', methods=('GET', 'POST'))
+def contact():
+    form = Sample()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
