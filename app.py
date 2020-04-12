5b50f98568086c3a0d658f897d61432a575e359f
Sylver11 <justus@cognician.com>
2020-04-12 21:47:49 +0200

    prototyping dynamic form

--- app.py
+++ app.py
@@ -1,23 +1,67 @@
-from flask import Flask, render_template, Response, request, session
+from flask import Flask, render_template, Response, request, session, jsonify
 from jinja2 import Template
-from forms import Sample, addTreatment
+from forms import Sample, addTreatment, ExpensesForm, MyForm, NewOrderForm
+import json
 
 app = Flask(__name__)
-
-
 
 
 @app.route('/', methods=('GET', 'POST'))
 def contact():
     form = Sample()
-    if form.validate_on_submit():
-        return redirect(url_for('success'))
+    #if form.validate_on_submit():
+     #   if form.flist.data:
+          #  for item in form.flist.data:
+                    # do stuff
+
     return render_template('index.html', form=form)
 
 
+@app.route('/add-product', methods=['POST'])
+def add_product():
+    form = NewOrderForm()
+    if form.quantity.data:
+        for item in form.quantity.data:
+            newOrder = Order(qty = item)
+            db.session.add(newOrder)
+            db.session.commit()
+        return jsonify(result='success')
+    return jsonify(result='error')
+
+@app.route('/add-product-test')
+def add_product_test():
+    form = NewOrderForm()
+    set_choices = [(set.id, set.friendly_name)
+    for set in Set.query.order_by(Set.friendly_name).all()]
+    return render_template('index.html', form=form, set_choices=set_choices)
+
+@app.route("/newqualification/<welder_id>", methods=['GET', 'POST'])
+def newqualification(welder_id=None):
+    form = MyForm()
+
+        #writemethod.
+
+    return render_template('index.html', title='xyz', form=form)
+
+@app.route("/modifywps", methods=['POST', 'GET'])
+def modifywps():
+    application_standard_id = request.args.get('std') # gets value from the getJson()
+
+   # process it however you want.. 
+
+    return
+
+
 @app.route('/add-treatment',methods=('GET','POST'))
-def adding(key):
-    addTreatment(key)
+def adding():
+    response = request.get_json()
+    print(response)
+    a,b,c = [response[k] for k in ('a', 'b','c')]
+    message = response.values()
+    addTreatment(response)
+    print(a)
+    return jsonify(response)
+  #  addTreatment(key)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,9 +1,52 @@
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms.fields.html5 import DateField
-from wtforms import StringField, TextField, SubmitField, FieldList, SelectField
+#from wtforms.fields.html5 import DateField
+from wtforms import StringField, TextField, SubmitField, validators, FieldList, SelectField, FormField, FloatField, DateField 
 from wtforms.validators import DataRequired, Length, Email, Required
+from wtforms import Form as NoCsrfForm
+import datetime
+import wtforms_json
 
 
+
+wtforms_json.init()
+
+
+
+class MyForm(FlaskForm):
+    standard = StringField('Expense_Item', validators=[DataRequired()])
+    wps = StringField('Number')
+ 
+#record = {'field1': 'label1', 'field2': 'label2'}
+
+#for key, value in record.items():
+#    setattr(MyForm, key, SelectField("Treatment", choices = value, validators=[Required()]))
+
+class NewOrderForm(FlaskForm):
+    #product = FieldList(StringField())
+    set = SelectField('Set', coerce=int)
+    qty = StringField('Quantity', validators=[DataRequired()])
+
+    submit = SubmitField('Submit')
+
+    def __init__(self, *args, **kwargs):
+        super(NewOrderForm, self).__init__(*args, **kwargs)
+        self.set.choices = [(set.id, set.friendly_name) for set in Set.query.order_by(Set.friendly_name).all()]
+
+
+
+class ExpenseItem(NoCsrfForm):
+    expense_name = StringField('Expense_Item', validators=[DataRequired()])
+    cost = FloatField('Cost', validators=[DataRequired()])
+    due_date = DateField('Due Date', format='%Y-%m-%d', validators=[DataRequired()], default=datetime.datetime.today().date())
+    type = SelectField('Role', choices=[('mutual', 'Mutual'),('personal#1', 'Personal #1'),('personal#2', 'Personal #2'),])
+
+
+class ExpensesForm(FlaskForm):
+    """A collection of expense items."""
+    items = FieldList(FormField(ExpenseItem), min_entries=1)
+    #for item in items:
+     #   print(item.data['expense_name'], item.data['cost'])
+    #print(items)
 
 class Sample(FlaskForm):
     name = StringField()
@@ -15,5 +58,15 @@ class Sample(FlaskForm):
 
 
 
-def addTreatment(key):
-    setattr(Sample, key, SelectField("Treatment", choices = myChoices, validators=[Required()]))
+def addTreatment(response):
+    a, b, c = [response[k] for k in ("a", "b", "c")]
+    #mylist = {}
+    #mylist[a] = b
+    #print(mylist)
+    myChoices = Sample()
+    print(response)
+    for key, value in response.items():
+        setattr(Sample, key, StringField(value))
+
+
+   # setattr(Sample, str(ke, SelectField("Treatment", choices = myChoices, validators=[Required()]))
