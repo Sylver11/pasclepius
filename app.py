deea67a4ec31af3e3ea412a2ffb3856e30b8948e
Sylver11 <justus@cognician.com>
2020-04-13 18:15:48 +0200

    working routing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request, session, jsonify
+from flask import Flask, render_template, Response, request, session, jsonify, redirect, url_for
 from jinja2 import Template
 from forms import Patient, Treatment
 import json
@@ -19,35 +19,47 @@ def contact():
 
 
 @app.route('/patient', methods=('GET', 'POST'))
-def newInvoice():
+def findPatient():
     form = Patient()
-    #if form.validate_on_submit():
-     #   if form.flist.data:
-          #  for item in form.flist.data:
-                    # do stuff
-
     return render_template('patient.html', form=form)
 
 
+@app.route('/patient/select', methods=('GET', 'POST'))
+def selectPatient():
+    form = Patient()
+    if form.validate_on_submit():
+        session["PATIENT"] = request.values
+        jsonData = jsonify(data={"medical": str(form.medical.data), 'name': str(form.name.data),'case': str(form.case.data), 'po': str(form.po.data), 'date': str(form.date.data) })
+        return jsonData
+    return jsonify(data=form.errors)
 
-@app.route('/add-product', methods=['POST'])
-def add_product():
-    form = Treatment()
-    if form.quantity.data:
-        for item in form.quantity.data:
-            newOrder = Order(qty = item)
-            db.session.add(newOrder)
-            db.session.commit()
-        return jsonify(result='success')
-    return jsonify(result='error')
 
-@app.route('/invoice/patient/<')
-def add_product_test():
-    form = Treatment()
-    set_choices = [('mutual', 'Mutual'),('personal#1', 'Personal #1'),('personal#2', 'Personal #2')]
+@app.route('/patient/<patient>/new-invoice')
+def newInvoice(patient):
+    form = Treatment() 
+    set_choices = [(105, 'Muscle and nerve stimulating currents'),(301,'Percussion'),(314, 'Lymph drainage')]
     #[(set.id, set.friendly_name)
     #for set in Set.query.order_by(Set.friendly_name).all()]
-    return render_template('index.html', form=form, set_choices=set_choices)
+    return render_template('index.html', form=form, set_choices=set_choices, patient = patient, po = session.get('PATIENT')["po"], case = session.get('PATIENT')["case"], date = session.get('PATIENT')["date"], medical = session.get('PATIENT')['medical'])
+
+
+@app.route('/session')
+def sessionValues():
+      return str(session.get('PATIENT')["po"])
+
+@app.route('/generate-invoice', methods=['POST'])
+def generateInvoice():
+    form = Treatment()
+    print(request.json)
+    if form.set.data:
+       # print(form.set.data)
+        for item in range(len(form.set.data)):
+            print(form.set.data[item])
+           # newOrder = Order(qty = item)
+           # db.session.add(newOrder)
+           # db.session.commit()
+        return jsonify(result='success')
+    return jsonify(result='error')
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms.fields.html5 import DateField
-from wtforms import StringField, TextField, SubmitField, validators, FieldList, SelectField, FormField, FloatField
+from wtforms import StringField, IntegerField, TextField, SubmitField, validators, FieldList, SelectField, FormField, FloatField
 from wtforms.validators import DataRequired, Length, Email, Required
 from wtforms import Form as NoCsrfForm
 import datetime
@@ -9,7 +9,7 @@ import wtforms_json
 wtforms_json.init()
 
 class Treatment(FlaskForm):
-    set = SelectField('Set', choices=[('mutual', 'Mutual'),('personal#1', 'Personal #1'),('personal#2', 'Personal #2'),])
+    set = SelectField('Set', choices=[(314, 'Lymph drainage'),(305, 'Re-education of movement/ Exercises (excluding ante- and post-natal exercises)'),(131, 'Ante and post natal exercises/counselling'),])
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()], default=datetime.datetime.today().date())
 
    # qty = StringField('Quantity', validators=[DataRequired()])
@@ -26,6 +26,6 @@ class Patient(FlaskForm):
     medical = SelectField('Medical Aid', choices = medicalAid, validators = [Required()])
     name = StringField(u'Full Name', validators=[DataRequired()])
     case = StringField(u'Case Number', validators=[DataRequired()])
-    po = FloatField(u'PO', validators=[DataRequired(), Length(6)])
+    po = IntegerField(u'PO', validators=[DataRequired()])
     date =  DateField(u'Invoice Date', format='%Y-%m-%d', validators=[DataRequired()], default=datetime.datetime.today().date())
     submit = SubmitField('Continue')
