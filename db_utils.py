9b98af17aea7587639097e6be03c736be9bbfa6c
Arthur Brophy <artbrophy@gmail.com>
2020-04-23 21:54:44 +0200

    review

--- db_utils.py
+++ db_utils.py
@@ -1,7 +1,7 @@
 import pymysql
 from DBUtils.PooledDB import PooledDB
 # Connect to the database
-#connection = pymysql.connect(host='localhost',
+# connection = pymysql.connect(host='localhost',
 #                             user='root',
 #                             password='Sylvester12.',
 #                             db='pasclepius',
@@ -10,26 +10,19 @@ from DBUtils.PooledDB import PooledDB
 
 
 
+# you shouldn't ever commit credentials to git
+# rather set up environment variables that store these values where ever
+# you run the server
 pool = PooledDB(creator = pymysql,
-
                                host= 'localhost',
-
                                user= 'root',
-
                                password='Sylvester12.',
-
                                database='pasclepius',
-
                                autocommit=True,
-
                                charset='utf8mb4',
-
                                cursorclass=pymysql.cursors.DictCursor,
-
                                blocking=False,
-
                                maxconnections=10)
-
 
 #connection = mySQLConnectionPool.connection()
 
