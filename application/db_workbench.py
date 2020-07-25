e187dc285f76da1cdd62540feabe80d1c664175a
Sylver11 <justus.sylvester@hotmail.de>
2020-07-25 23:38:21 +0200

    if entry already exist replace. query for tab solution

--- application/db_workbench.py
+++ application/db_workbench.py
@@ -3,8 +3,12 @@ from application.db_utils import pool
 def newWork(uuid_text, work_type, work_quality):
     conn = pool.connection()
     cursor = conn.cursor()
-    cursor.execute(""""INSERT INTO usr_workbench (uuid_text, work_type,
-    work_quality) VALUE ('{0}', '{1}', '{2}')""".format(uuid_text, work_type, work_quality))
+    sql_delete = """DELETE FROM user_workbench WHERE uuid_text = '{0}' AND
+    work_type = '{1}' AND work_quality = '{2}'""".format(uuid_text, work_type, work_quality)
+    cursor.execute(sql_delete)
+    sql = """INSERT INTO user_workbench (uuid_text, work_type,
+    work_quality) VALUES ('{0}', '{1}', '{2}')""".format(uuid_text, work_type, work_quality)
+    cursor.execute(sql)
     cursor.close()
     conn.close()
     return 'success'
